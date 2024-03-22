7a362855f9ce5cfbfad21e6b2a8a538d689569c9
GhilesHideur <47829666+GhilesHideur@users.noreply.github.com>
2024-03-22 17:58:25 +0100

    fix: pillow image tutorial

--- 09_pillow_image.py
+++ 09_pillow_image.py
@@ -1,11 +1,21 @@
 from taipy.gui import Gui
 from chlkt import *
-
+import io
+import base64
 from pathlib import Path
-
 from PIL import Image, ImageOps
 
-gui = Gui()
+image_path = Path(__file__).parent.resolve() / "spring_image.jpg"
+image = Image.open(image_path)
+
+# Convert the image to a byte stream
+byte_stream = io.BytesIO()
+image.save(byte_stream, format=image.format)
+
+# Encode the byte stream to Base64
+base64_string = base64.b64encode(byte_stream.getvalue()).decode("utf-8")
+greyscale_intensity = 1
+
 
 def convert_to_grayscale(image, intensity):
     """Convert an image to grayscale with a given intensity."""
@@ -13,21 +23,21 @@ def convert_to_grayscale(image, intensity):
         intensity = 0
     if intensity > 1:
         intensity = 1
-    img_grey = ImageOps.grayscale(image).point(lambda x: x * intensity + 255 * (1 - intensity))
+    img_grey = ImageOps.grayscale(image).point(
+        lambda x: x * intensity + 255 * (1 - intensity)
+    )
     return img_grey
 
-image_path = Path(__file__).parent.resolve() / "XN_Fruehjahrswiese_00.jpg"
-
-image = Image.open(image_path)
 
 img_grey = convert_to_grayscale(image, 1)
 
-greyscale_intensity = 1
 
 def on_change(state, var, val):
-    if (var=="greyscale_intensity"):
+    if var == "greyscale_intensity":
         state.img_grey = convert_to_grayscale(state.image, val)
 
+
 page = ChalkitPage("09_pillow_image.xprjson")
+gui = Gui()
 gui.add_page("page", page)
 gui.run(run_browser=True, use_reloader=False)
